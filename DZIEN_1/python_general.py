3349919b19eef55c01411a19aa90c747a22d8b37
albim72 <marcin.albiniak72@gmail.com>
2025-08-11 10:34:36 +0200

    Create python_general.py

---
+++ DZIEN_1/python_general.py
@@ -0,0 +1,9 @@
+import os
+from dataclasses import dataclass
+from typing import Iterable,Callable
+from collections import defaultdict,Counter
+from functools import reduce
+from itertools import groupby
+from operator import attrgetter
+
+print(os.getcwd())
