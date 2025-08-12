f880078e19efcc6cf5013b74c4d8e41da35a3c14
albim72 <marcin.albiniak72@gmail.com>
2025-08-12 13:12:15 +0200

    Update marketing.py

--- DZIEN_2/marketing_analiza/marketing.py
+++ DZIEN_2/marketing_analiza/marketing.py
@@ -9,3 +9,9 @@ data_dir = Path("data")
 sales_file = data_dir / "sales.csv"
 marketing_file = data_dir / "markieting.xlsx"
 currencies_file = data_dir / "currencies.json"
+
+#wczytywanie danych
+sales_df = pd.read_csv(sales_file,parse_dates=["date"])
+marketing_df = pd.read_excel(marketing_file,parse_dates=["date"])
+with open(currencies_file,"r",encoding="utf-8") as f:
+    currencies_data = json.load(f)
