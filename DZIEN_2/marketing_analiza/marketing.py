a983c918babced93a2d6ae88ef4bb438cff47d52
albim72 <marcin.albiniak72@gmail.com>
2025-08-12 13:09:09 +0200

    Update marketing.py

--- DZIEN_2/marketing_analiza/marketing.py
+++ DZIEN_2/marketing_analiza/marketing.py
@@ -4,3 +4,8 @@ import matplotlib.pyplot as plt
 import json
 from pathlib import Path
 
+#ścieżki do plików
+data_dir = Path("data")
+sales_file = data_dir / "sales.csv"
+marketing_file = data_dir / "markieting.xlsx"
+currencies_file = data_dir / "currencies.json"
