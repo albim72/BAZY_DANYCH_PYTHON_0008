90eb274fa56ead8841901fec0b6cc4da16f5d4ce
albim72 <marcin.albiniak72@gmail.com>
2025-08-12 14:07:02 +0200

    Create pandas_sql.py

---
+++ DZIEN_2/pandas_sql.py
@@ -0,0 +1,60 @@
+from pathlib import Path
+import sqlite3
+import pandas as pd
+
+DB_PATH = Path("shop.db")
+
+def init_db(db_path: Path):
+    conn = sqlite3.connect(db_path)
+    cur = conn.cursor()
+    cur.executescript("""
+    DROP TABLE IF EXISTS customers;
+    DROP TABLE IF EXISTS orders;
+    
+    CREATE TABLE customers
+    (
+        customer_id INTEGER PRIMARY KEY,
+        name TEXT NOT NULL,
+        country TEXT NOT NULL
+    );
+
+    CREATE TABLE orders
+    (
+        order_id INTEGER PRIMARY KEY,
+        customer_id INTEGER NOT NULL,
+        order_date TEXT NOT NULL,
+        amount REAL NOT NULL,
+        FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+    );
+        
+        
+    """)
+    
+    customers = [
+        (1,"Anna Kowalska","PL"),
+        (2,"John Smith","US"),
+        (3,"Hans Muller","DE"),
+        (4,"Karol Wiśniak","PL"),
+        (5,"Marie Dubois","FR"),
+        (6,"Natan Wote","GB"),
+    ]
+    
+    orders = [
+        (101,1,"2025-01-10",245.0),
+        (102,1,"2025-01-11",356.0),
+        (103,2,"2025-01-17",112.0),
+        (104,3,"2025-01-12",1130.0),
+        (105,4,"2025-01-13",789.0),
+        (106,5,"2025-01-12",88.0),
+        (107,5,"2025-01-18",321.0),
+        (108,5,"2025-01-21",555.0),
+        (109,1,"2025-01-22",975.0),
+        (110,6,"2025-01-21",23.0),
+        (111,6,"2025-01-27",450.0),
+    ]
+    
+    cur.executemany("INSERT INTO customers VALUES (?,?,?)", customers)
+    cur.executemany("INSERT INTO orders VALUES (?,?,?,?)", orders)
+    
+    conn.commit()
+    return conn
