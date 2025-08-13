9ce5e45935e6d00ca3660572b7b55ca154212634
albim72 <marcin.albiniak72@gmail.com>
2025-08-13 12:42:29 +0200

    Create pandas_sql.py

---
+++ DZIEN_3/SQL/pandas_sql.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+# Dane logowania do MySQL
+MYSQL_USER = "user"
+MYSQL_PASS = "password"
+MYSQL_HOST = "127.0.0.1"
+MYSQL_PORT = 3306
+MYSQL_DB   = "demo_db"
+
+# Tworzymy silnik SQLAlchemy
+engine = create_engine(
+    f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASS}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
+)
+
+# ----- ZAPIS -----
+# Tworzymy mały DataFrame
+df = pd.DataFrame({
+    "id": [1, 2, 3],
+    "name": ["Aki", "Mika", "Ren"],
+    "score": [88, 92, 79]
+})
+
+# Zapisujemy do MySQL
+df.to_sql("simple_table", con=engine, if_exists="replace", index=False)
+
+print("Tabela zapisana do MySQL.")
+
+# ----- ODCZYT -----
+# Wczytujemy tabelę z MySQL do DataFrame
+df_read = pd.read_sql("SELECT * FROM simple_table", con=engine)
+print("Odczytane dane:")
+print(df_read)
